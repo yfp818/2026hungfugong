4a2bab825c0bce562ef2ad399e24a8af643a292c
yfp818 <yfp818@example.com>
2026-07-07 13:07:13 +0800

    bording

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,7 @@ const nextConfig: NextConfig = {
   // 1. 保留您原本的區網測試設定
   allowedDevOrigins: ['172.20.10.2'],
 
-  // 2. 忽略 ESLint 語法警告，強行通關
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // 3. 忽略 TypeScript 型別錯誤，強制打包！
+  // 2. 忽略 TypeScript 型別錯誤，強制打包！(這個無敵星星依然有效)
   typescript: {
     ignoreBuildErrors: true,
   },
